2f7cf72831986371be7dfa6c19429b2e77af2a1f
KaykyAmorim <kaykyusjt@gmail.com>
2026-03-03 21:40:41 -0300

    Add files via upload

---
+++ P.Calvetti/VelocidadeMedia.java
@@ -0,0 +1,13 @@
+/*Elabore um algoritmo e um programa Java
+que receba a distância percorrida em km e o tempo gasto em hora o programa
+deve exibir a velocidade média.*/   
+
+public class VelocidadeMedia {
+    public static void main (String[]args) {
+        int x = 10, y = 5;
+        int distancia = x;
+        int tempo = y;
+        int VelocidadeMedia = x / y ;
+        System.out.println (distancia "/"  tempo = + VelocidadeMedia);
+    }
+}
