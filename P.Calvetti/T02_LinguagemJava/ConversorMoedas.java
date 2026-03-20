2a4d97e804a7cd18c6981ee4756817f50b261eba
Kayky Amorim <kaykyusjt@gmail.com>
2026-03-19 21:28:07 -0300

    Melhoria

--- P.Calvetti/T02_LinguagemJava/ConversorMoedas.java
+++ P.Calvetti/T02_LinguagemJava/ConversorMoedas.java
@@ -9,14 +9,11 @@ public class ConversorMoedas{
         Scanner scanner = new Scanner(System.in);
 
         double reais;
-        double cotacaoDollar;
+        double cotacaoDollar = 5.22;
         double quantidadeDollar;
 
         System.out.println("Digite a quantidade que você tem em reais: ");
         reais = scanner.nextDouble();
-
-        System.out.println("Digite o valor do dollar atualmente: ");
-        cotacaoDollar = scanner.nextDouble();
 
         quantidadeDollar = reais * cotacaoDollar;
 
